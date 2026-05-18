54748e833fba4503763b2f11a5a3fd1608be7887
Ellynhenmateo <141325013+Ellynhenmateo@users.noreply.github.com>
2026-05-19 02:28:42 +0800

    Update listing controller routes

---
+++ App/controllers/ListingController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+use PDO;
+
+
+class ListingController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+
+        $this->db = new Database($config);
+    }
+
+    public function index()
+    {
+
+        $listings = $this->db->query("SELECT * FROM listings ")->fetchAll(PDO::FETCH_OBJ);
+
+
+        loadView('listings/index', ['listings' => $listings]);
+    }
+}
