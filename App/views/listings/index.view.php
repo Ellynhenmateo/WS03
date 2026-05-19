43627a9b562f21c4b1972122fa38f8ad924f0411
Ellynhenmateo <141325013+Ellynhenmateo@users.noreply.github.com>
2026-05-19 19:28:27 +0800

    Implement job listing search functionality

--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -6,7 +6,12 @@
 <section>
     <div class="container mx-auto p-4 mt-4">
         <?= LoadPartial('message') ?>
-        <div class=" text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
+        <div class=" text-center text-3xl mb-4 font-bold border border-gray-300 p-3">
+            <?php if(isset($keywords)) :?> Search Results for: <?= htmlspecialchars($keywords) ?>
+                <?php else :?>
+All Jobs
+        <?php endif ;?>
+            </div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <?php foreach ($listings as $listing): ?>
             <!-- Job Listing 1: Software Engineer -->
